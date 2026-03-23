08a0843b5d93adb96ad96efa297fbd19a51f5850
gustagueimer <gustavomarqueslima@outlook.com>
2026-03-23 16:53:15 -0300

    feat: adição de uma função copyWith() na entidade de produto

--- lib/features/product/domain/entities/product.dart
+++ lib/features/product/domain/entities/product.dart
@@ -13,6 +13,22 @@ class Product {
     this.fav = false,
   });
 
+  Product copyWith(
+    int? id,
+    String? title,
+    double? price,
+    String? image,
+    bool? fav
+  ){
+    return Product(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      price: price ?? this.price,
+      image: image ?? this.image,
+      fav: fav ?? this.fav
+    );
+  }
+
   @override
   String toString() {
     return "Product(id: $id, title: $title, price: $price, image: $image, fav: $fav);";
